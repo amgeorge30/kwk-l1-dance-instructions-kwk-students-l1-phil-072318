d29de217a194ecc5ca8193b5c7e04a1fca34100d
Anna George <amgeorge30@hotmail.com>
2018-07-23 20:17:00 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -11,8 +11,6 @@ def lower_body
   puts "Finishing with a small skip-step backward"
 end
 
-starting_stance
-lower_body
 def skip_step_left
   puts "Lower left foot to the ground"
   puts "Bounce left foot back up slightly, kicking it a few inches back"
@@ -24,19 +22,22 @@ def skip_step_right
   puts "Bounce right foot back up slightly, kicking it a few inches back"
   puts "Right, Left, Right and Right"
 end
-skip_step_left
-skip_step_right
 
 def bob_the_reins
   puts "Holding your arms out in front of you, straight and at chest level"
   puts "Cross your right wrist over your left and hold them together"
   puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
 end
-bob_the_reins
+
 
 def lasso 
   puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
   puts "Lift right arm up and point right elbow diagonally to the right."
   puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
 end
-lasso
+
+starting_stance
+lower_body
+skip_step_right
+skip_step_left
+bob_the_reins
