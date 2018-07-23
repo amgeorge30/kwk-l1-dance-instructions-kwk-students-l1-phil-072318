5a4bfc2b78f610a9026f0836b8d0e211a00833d3
Anna George <amgeorge30@hotmail.com>
2018-07-23 20:00:02 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -5,4 +5,4 @@ def Lower_body
   puts "Plant legs far apart, bend knees slightly and keep posture loose"
   puts "Lift right foot"
   puts "Return right foot to the ground"
-  puts ""
+  puts "Finishing with a small skip-step backward"
