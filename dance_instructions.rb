5ac734fcc5aa536157ca4ac48bc3c820c13748b6
Anna George <amgeorge30@hotmail.com>
2018-07-23 20:11:38 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -3,11 +3,14 @@
 
 def starting_stance
   puts "Plant legs far apart, bend knees slightly and keep posture loose"
+end
+
 def lower_body
   puts "Lift right foot"
   puts "Return right foot to the ground"
   puts "Finishing with a small skip-step backward"
 end
+
 starting_stance
 lower_body
 def skip_step
