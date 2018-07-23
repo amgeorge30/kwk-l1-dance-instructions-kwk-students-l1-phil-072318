f9cb4f19b480532ff3b1a4b2d2ee96cde91c202b
Anna George <amgeorge30@hotmail.com>
2018-07-23 20:20:20 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -11,11 +11,10 @@ def lower_body
   puts "Finishing with a small skip-step backward"
 end
 
-def skip_step_left
+def skip_step
   puts "Lower left foot to the ground"
   puts "Bounce left foot back up slightly, kicking it a few inches back"
   puts "Left, Right, Left and Left"
-
   puts "Lower right foot to the ground"
   puts "Bounce right foot back up slightly, kicking it a few inches back"
   puts "Right, Left, Right and Right"
